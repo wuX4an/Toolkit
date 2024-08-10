c136d72a6122285f8549957a0e661acf999551e9
wux4an <wux4an@tutanota.com>
2024-08-10 14:54:52 -0500

    Better error handling for the upload files function

--- bin/main.dart
+++ bin/main.dart
@@ -24,30 +24,35 @@ void main(List<String> args) async {
 }
 
 Future<void> uploadFile(File file) async {
-  final boundary = '--${DateTime.now().millisecondsSinceEpoch}';
+  try {
+    final boundary = '--${DateTime.now().millisecondsSinceEpoch}';
 
-  final request =
-      await HttpClient().postUrl(Uri.parse('https://tmpfiles.org/api/v1/upload'));
-  request.headers
-      .set(HttpHeaders.contentTypeHeader, 'multipart/form-data; boundary=$boundary');
+    final request =
+        await HttpClient().postUrl(Uri.parse('https://tmpfiles.org/api/v1/upload'));
+    request.headers
+        .set(HttpHeaders.contentTypeHeader, 'multipart/form-data; boundary=$boundary');
 
-  final buffer = StringBuffer();
-  buffer.writeln(
-      '--$boundary\r\nContent-Disposition: form-data; name="file"; filename="${file.path}"\r\nContent-Type: application/octet-stream\r\n\r\n');
-  buffer.write(await file.readAsString());
-  buffer.writeln('\r\n--$boundary--\r\n');
+    final buffer = StringBuffer();
+    buffer.writeln(
+        '--$boundary\r\nContent-Disposition: form-data; name="file"; filename="${file.path}"\r\nContent-Type: application/octet-stream\r\n\r\n');
+    buffer.write(await file.readAsString());
+    buffer.writeln('\r\n--$boundary--\r\n');
 
-  request.add(utf8.encode(buffer.toString()));
-  final response = await request.close();
+    request.add(utf8.encode(buffer.toString()));
+    final response = await request.close();
 
-  if (response.statusCode == 200) {
-    final data = await response.transform(utf8.decoder).join();
-    final json = JsonDecoder().convert(data);
-    print("+ Status: ${json['status']}");
-    print("+ Url: ${json['data']['url']}");
-    exit(0);
-  } else {
-    print('Error uploading file: ${response.statusCode}');
+    if (response.statusCode == 200) {
+      final data = await response.transform(utf8.decoder).join();
+      final json = JsonDecoder().convert(data);
+      print("+ Status: ${json['status']}");
+      print("+ Url: ${json['data']['url']}");
+      exit(0);
+    } else {
+      print('Error uploading file: ${response.statusCode}');
+      exit(0);
+    }
+  } catch (e) {
+    print(e);
     exit(0);
   }
 }
